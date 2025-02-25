c0971a9cf0d6f601f945352872384397efc13b18
Jake Lee <l.junwoo@digipen.edu>
2025-02-25 13:51:28 +0900

    witch_attack(WIP)

--- GameJam WINTER/witch.cpp
+++ GameJam WINTER/witch.cpp
@@ -1,9 +1,29 @@
 #include"witch.h"
+#include"math.h"
 
 void Witch::Witch_Draw() {
-	DrawRectangle(Witch_Pos_X, Witch_Pos_Y, Witch_Width, Witch_Height, Witch_color);
+    DrawRectangle(Witch_Pos_X, Witch_Pos_Y, Witch_Width, Witch_Height, Witch_color);
 }
 
-void Witch::Witch_Move() {
-	
+void Witch::Witch_Move(Vector2 target) {
+    float dx = target.x - Witch_Pos_X;
+    float dy = target.y - Witch_Pos_Y;
+    float distance = sqrt(dx * dx + dy * dy);
+    if (distance != 0) {
+        Witch_Pos_X += (dx / distance) * Witch_Speed;
+        Witch_Pos_Y += (dy / distance) * Witch_Speed;
+    }
+}
+
+Rectangle Witch::Get_Hit() {
+    
+}
+
+void Witch::Witch_Attack(bool Collision) {
+    DrawCircle(Witch_AT_X, Witch_AT_Y, Witch_AT_R, Witch_AT_C);
+    if (Collision) {
+        Witch_AT_R *= 2;
+        Witch_AT_C = BLACK;
+        Collision = false;
+    }
 }
--- GameJam WINTER/witch.h
+++ GameJam WINTER/witch.h
@@ -5,13 +5,23 @@ class Witch {
 public:
 	float Witch_Pos_X = 50;
 	float Witch_Pos_Y = 200;
-	
+
 	void Witch_Draw();
-	void Witch_Move();
+	void Witch_Move(Vector2 target);
+	void Witch_Attack(bool Collision); //collision with player or castle
+
+	//Witch attack
+	float Witch_AT_X = Witch_Pos_X;
+	float Witch_AT_Y = Witch_Pos_Y;
+	float Witch_AT_R = 1;
+	float Witch_AT_S = 2;
+	Color Witch_AT_C = RED;
 
 private:
+	Rectangle Get_Hit();
 	float Witch_Width = 50;
 	float Witch_Height = 50;
-	
+	float Witch_Speed = 0.1;
+
 	Color Witch_color = PURPLE;
 };
