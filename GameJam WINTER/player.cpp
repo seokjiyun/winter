7821dea2ad0b0cf25ced42a0423192fe2749f345
Jiyun-Seok-digipen <80096094+seokjiyun@users.noreply.github.com>
2025-02-25 15:05:58 +0900

    attack

--- GameJam WINTER/player.cpp
+++ GameJam WINTER/player.cpp
@@ -1,7 +1,31 @@
 #include"player.h"
+#include <math.h>
 
 void Player::Player_Move()
 {
+	Vector2 moveDir = { 0, 0 };
+
+	if (IsKeyDown(KEY_UP)) {
+		moveDir.y -= 1;
+	}
+
+	if (IsKeyDown(KEY_DOWN)) {
+		moveDir.y += 1;
+	}
+
+	if (IsKeyDown(KEY_RIGHT)) {
+		moveDir.x += 1;
+	}
+
+	if (IsKeyDown(KEY_LEFT)) {
+		moveDir.x -= 1;
+	}
+
+	if (moveDir.x != 0 || moveDir.y != 0) {
+		float length = sqrt(moveDir.x * moveDir.x + moveDir.y * moveDir.y);
+		facingDirection = { moveDir.x / length, moveDir.y / length };
+	}
+
 	if (IsKeyDown(KEY_UP)) {
 		Player_Pos_Y -= Player_Speed;
 	}
@@ -29,8 +53,36 @@ void Player::Player_Draw() {
 
 
 Rectangle Player::GetAttackRect() const {
-	DrawRectangle( Player_Pos_X - 50, Player_Pos_Y, 50, 50,GREEN );
-	return{ Player_Pos_X - 50, Player_Pos_Y, 50, 50 };
+	float attackWidth = 50;
+	float attackHeight = 50;
+	float offsetX = 0;
+	float offsetY = 0;
+
+	
+	if (fabs(facingDirection.x) > fabs(facingDirection.y)) {
+	
+		if (facingDirection.x > 0) { 
+			offsetX = Player_R;
+			offsetY = (Player_R / 2) - (attackHeight / 2);
+		}
+		else { 
+			offsetX = -attackWidth;
+			offsetY = (Player_R / 2) - (attackHeight / 2);
+		}
+	}
+	else {
+		
+		if (facingDirection.y > 0) { 
+			offsetY = Player_R;
+			offsetX = (Player_R / 2) - (attackWidth / 2);
+		}
+		else { 
+			offsetY = -attackHeight;
+			offsetX = (Player_R / 2) - (attackWidth / 2);
+		}
+	}
+	DrawRectangle(Player_Pos_X + offsetX, Player_Pos_Y + offsetY, attackWidth, attackHeight, GREEN);
+	return { Player_Pos_X + offsetX, Player_Pos_Y + offsetY, attackWidth, attackHeight };
 }
 
 void Player::Attack(Gob& gob) {
--- GameJam WINTER/player.h
+++ GameJam WINTER/player.h
@@ -13,7 +13,9 @@ public:
 	float Player_Pos_X = 0;
 	float Player_Pos_Y = 0;
 	float Player_Speed = 3;
-	
+
+	Vector2 facingDirection = { 1, 0 };
+
 private:
 	float Player_R = 50;
 	Color Player_Color = BLACK;
